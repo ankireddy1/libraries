cdd6528043af817c78e6666e531e9508bb3cccc3
ankireddy1 <101354717+ankireddy1@users.noreply.github.com>
2022-04-19 22:06:44 +0530

    Update CI-CD.groovy

--- vars/CI-CD.groovy
+++ vars/CI-CD.groovy
@@ -6,5 +6,12 @@ def newMaven()
 {
   sh 'mvn package'
 }
-
+def newDeploy()
+{
+  deploy adapters: [tomcat9(credentialsId: '2f830b86-9f9e-4278-8c6d-9d0b3c0dd7af', path: '', url:"${ip}")], contextPath: "${appname}", war: '**/*.war'
+}
+def newTest()
+{
+  sh 'java -jar testing.jar'
+}
   
